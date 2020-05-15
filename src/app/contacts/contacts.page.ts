a30f59e0254b660ee622222094753c71fd5c7b5e
Miguel Angel <michelmatcas@hotmail.com>
2020-05-15 11:13:44 +0200

    Listar contactos

--- src/app/contacts/contacts.page.ts
+++ src/app/contacts/contacts.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../models/contact';
+import { ContactsProvider } from '../services/contacts.service';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.page.html',
   styleUrls: ['./contacts.page.scss'],
 })
+
 export class ContactsPage implements OnInit {
-
-  constructor() { }
-
+  private myContacts: Contact[];
+  constructor(private contacts: ContactsProvider) { }
   ngOnInit() {
+    console.log('ngOnInit ContactsPage');
+    this.listContacts();
   }
-
+  listContacts() {
+    console.log('[ContactsPage] listContacts()');
+    this.myContacts = this.contacts.listContacts().sort();
+  }
 }
+
